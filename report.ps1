0a7bd19d7112965ee7faab7eca882a1e9a1d32c7
Alec Clews <alecclews@gmail.com>
2020-08-30 15:26:51 +1000

    Added comment block

--- report.ps1
+++ report.ps1
@@ -1,3 +1,23 @@
+#requires -version 2
+<#
+.SYNOPSIS
+Report on PaperCut MF/NG Status
+.DESCRIPTION
+Displays a dump of PaperCut related information
+.PARAMETER <None>
+.INPUTS
+None
+.OUTPUTS
+Outputs Demo Report
+.NOTES
+Version:        0.1
+Author:         Alec Clews
+Creation Date:  2020/August
+Purpose/Change: Initial script development
+
+.EXAMPLE
+<Example goes here. Repeat this attribute for more than one example>:
+#>
 
 Get-Service -DisplayName *PaperCut* | Format-Table -Property  Name, Status, StartType
 
