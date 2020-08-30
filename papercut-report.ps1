fd658ba4291fe4c590850f30c30374574ee1e0c4
Alec Clews <alecclews@gmail.com>
2020-08-31 08:23:46 +1000

    Add more info and make it pretty

--- papercut-report.ps1
+++ papercut-report.ps1
@@ -10,7 +10,7 @@ None
 .OUTPUTS
 Outputs Demo Report
 .NOTES
-Version:        0.1
+Version:        0.2
 Author:         Alec Clews
 Creation Date:  2020/August
 Purpose/Change: Initial script development
@@ -19,5 +19,31 @@ Purpose/Change: Initial script development
 <Example goes here. Repeat this attribute for more than one example>:
 #>
 
-Get-Service -DisplayName *PaperCut* | Format-Table -Property  Name, Status, StartType
 
+Function server-command {
+    &'C:\Program Files\PaperCut MF\server\bin\win\server-command.exe' $Args
+}
+# Find the value of the Auth key in the PaperCut MF/NG config database
+$authString = server-command get-config health.api.key 
+
+# Set up the http header for the health API 
+$headers = @{'Authorization' = $authString}
+
+$uri = [Uri]"http://localhost:9191/api/health"
+
+# Generate the report
+&{
+  # Get a list of the processes running 
+  Get-Service -DisplayName  *PaperCut* | Select-Object -Property name,status  |
+            ConvertTo-Html -Fragment -PreContent '<h2>PaperCut Processes</h2>'
+
+  $rsp = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+
+  $rsp.applicationServer.systemInfo |
+  ConvertTo-Html -As List -Fragment -PreContent '<h2>PaperCut Services Info</h2>'
+
+  Write-Output "<p>Total Printers = $($rsp.printers.count)</p>"
+  Write-Output "<p>Total Devices = $($rsp.devices.count)</p>"
+ } | Out-File -FilePath .\report1.html
+
+ Invoke-Expression .\report1.html
